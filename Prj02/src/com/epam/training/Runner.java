f173b9157237ea348b739a2b4ae0f431da6dec88
Darya <dokape@mail.ru>
2018-11-07 23:10:47 +0300

    Prj02 add output to html file

--- Prj02/src/com/epam/training/Runner.java
+++ Prj02/src/com/epam/training/Runner.java
@@ -1,6 +1,9 @@
 package com.epam.training;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -10,7 +13,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Runner {
 
-	 static final Logger rootLogger = LogManager.getRootLogger();
+	static final Logger rootLogger = LogManager.getRootLogger();
 	static final Logger identicalLogger = LogManager
 			.getFormatterLogger("Identical");
 	static final Logger sameSingerLogger = LogManager
@@ -20,14 +23,14 @@ public class Runner {
 
 		String loggerPath = System.getProperty("user.dir")
 				+ "\\resources\\log4j2.xml";
-		
+
 		System.out.println(loggerPath);
 		System.setProperty("log4j.configurationFile", loggerPath);
 
-//		rootLogger.info("Test");
-//		identicalLogger.info("TESt");
-//
-//		sameSingerLogger.info("TESt2");
+		// rootLogger.info("Test");
+		// identicalLogger.info("TESt");
+		//
+		// sameSingerLogger.info("TESt2");
 		List<File> paths = new ArrayList();
 		if (args.length > 0) {
 			for (String s : args) {
@@ -40,9 +43,7 @@ public class Runner {
 			for (File dir : paths) {
 				mp3Files.addAll(MP3FileUtils.getFilesUnderDir(dir));
 			}
-			for (MP3File mp3 : mp3Files) {
-				System.out.println(mp3.toString());
-			}
+			printToHtml(mp3Files);
 
 		}
 
@@ -52,6 +53,23 @@ public class Runner {
 		return (s.contains("/")) || (s.contains("/")) || (s.contains(":"));
 	}
 
+	static void printToHtml(List<MP3File> mp3Files) {
+		String html = "";
+		for (MP3File mp3 : mp3Files) {
+			html += mp3.getTitle() + " - " + mp3.getSongPerformer()
+					+ "( <a href=\"" + mp3.getPath() + "\">" + mp3.getPath()
+					+ "</a>)<br/>";
+		}
+		try {
+			File file = new File(System.getProperty("user.dir")
+					+ "\\resources\\result.html");
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+			bw.write(html);
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	// private static List<Singer> singerConverter(List<MP3File> mp3files) {
 	//
 	// List<Singer> singers = new ArrayList<Singer>();
